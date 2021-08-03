47eb517d1bade141ea11e5cff7a7a1d65f60accd
bernardtran99 <bernardtran99@gmail.com>
2021-08-03 15:23:17 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -111,10 +111,10 @@ void flood(ndn_interest_t interest) {
         printf("%s\n", &router->pit->capacity);
         for(int i = 0; i < router->pit->capacity; i++) {
             //printf("looking at interfaces in pit");
-            ndn_table_id_t temp_pit_id = ndn_forwarder_get()->pit->slots[i].nametree_id;
-            nametree_entry_t *temp_nametree_entry = ndn_nametree_at(ndn_forwarder_get()->nametree, temp_pit_id);
+            ndn_table_id_t temp_pit_id = router->pit->slots[i].nametree_id;
+            nametree_entry_t *temp_nametree_entry = ndn_nametree_at(router->nametree, temp_pit_id);
             ndn_table_id_t temp_fib_id = temp_nametree_entry->fib_id;
-            ndn_fib_unregister_face(layer1_fib, NULL);
+            ndn_fib_unregister_face(layer1_fib, temp_fib_id);
         }
         router->fib = layer1_fib;
         ndn_forwarder_express_interest_struct(&interest, NULL, NULL, NULL);
