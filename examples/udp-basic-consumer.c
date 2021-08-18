36a339c218226fbbabee0038cb2c1e3c1a3f6777
bernardtran99 <bernardtran99@gmail.com>
2021-08-18 13:26:28 -0400

    Update udp-basic-consumer.c

--- examples/udp-basic-consumer.c
+++ examples/udp-basic-consumer.c
@@ -141,8 +141,7 @@ main(int argc, char *argv[])
   //ndn_interest_set_Parameters(&interest, (uint8_t*)time_ptr, sizeof(timestamp));
   printf("%d\n", time_ptr);
   printf("%d\n", selector[1]);
-  ndn_interest_set_Parameters(&interest, (uint8_t*)(selector_ptr), sizeof(selector[1]));
-  ndn_interest_set_Parameters(&interest, (uint8_t*)(selector_ptr), sizeof(selector[1]));
+  ndn_interest_set_Parameters(&interest, (uint8_t*)(selector_ptr + 8), sizeof(selector[1]));
   uint8_t test = interest.parameters.value[0];
   printf("%d\n", test);
 
