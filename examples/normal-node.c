340753edc7d430ba7fa3ed1d375f8bd1a768fb76
bernardtran99 <bernardtran99@gmail.com>
2021-08-18 19:20:42 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -224,6 +224,7 @@ void populate_fib() {
     port1 = htons((uint16_t) ul_port);
     ul_port = strtoul(sz_port2, NULL, 10);
     port2 = htons((uint16_t) ul_port);
+    printf("Here\n");
     ndn_name_from_string(&prefix_name, ancmt_string, strlen(ancmt_string));
     face = ndn_udp_unicast_face_construct(INADDR_ANY, port1, server_ip, port2);
     ndn_forwarder_add_route_by_name(&face->intf, &prefix_name);
