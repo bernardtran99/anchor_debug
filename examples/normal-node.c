898ee0c550af895096fc707f8fe3d0f2483d1259
bernardtran99 <bernardtran99@gmail.com>
2021-08-18 19:21:51 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -213,10 +213,11 @@ void populate_fib() {
     uint32_t ul_port;
     struct hostent *host_addr;
     struct in_addr **paddrs;
-printf("Here\n");
+
     sz_port1 = "3000";
     sz_addr = "rpi2-btran";
     sz_port2 = "5000";
+    printf("Here\n");
     host_addr = gethostbyname(sz_addr);
     paddrs = (struct in_addr **)host_addr->h_addr_list;
     server_ip = paddrs[0]->s_addr;
