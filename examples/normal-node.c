da84f100c93ef3e828f73128ecc2caad7dcf6884
bernardtran99 <bernardtran99@gmail.com>
2021-08-18 19:54:17 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -184,8 +184,8 @@ void send_ancmt() {
     ndn_key_storage_get_empty_ecc_key(&ecc_secp256r1_pub_key, &ecc_secp256r1_prv_key);
     ndn_ecc_make_key(ecc_secp256r1_pub_key, ecc_secp256r1_prv_key, NDN_ECDSA_CURVE_SECP256R1, 890);
     ndn_ecc_prv_init(ecc_secp256r1_prv_key, secp256r1_prv_key_str, sizeof(secp256r1_prv_key_str), NDN_ECDSA_CURVE_SECP256R1, 0);
-    // ndn_key_storage_t *storage = ndn_key_storage_get_instance();
-    // ndn_name_t *self_identity_ptr = storage->self_identity;
+    ndn_key_storage_t *storage = ndn_key_storage_get_instance();
+    ndn_name_t *self_identity_ptr = storage->self_identity;
     // //TODO: segmentation fault here
     // ndn_signed_interest_ecdsa_sign(ancmt, self_identity_ptr, ecc_secp256r1_prv_key);
     // encoder_init(encoder, interest_buf, 4096);
