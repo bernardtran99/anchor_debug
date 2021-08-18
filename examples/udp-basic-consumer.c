0b68774fb3d860270c4969234ea30fb39f2244e7
bernardtran99 <bernardtran99@gmail.com>
2021-08-17 20:39:41 -0400

    Update udp-basic-consumer.c

--- examples/udp-basic-consumer.c
+++ examples/udp-basic-consumer.c
@@ -139,7 +139,7 @@ main(int argc, char *argv[])
 
   //ndn_interest_set_Parameters(&interest, (uint8_t*)time_ptr, sizeof(timestamp));
   printf("%s\n", time_ptr);
-  ndn_interest_set_Parameters(&interest, &selector[1], sizeof(selector[1]));
+  ndn_interest_set_Parameters(&interest, selector[1], sizeof(selector[1]));
   printf("%s\n", selector[1]);
   ndn_forwarder_express_interest_struct(&interest, on_data, on_timeout, NULL);
 
