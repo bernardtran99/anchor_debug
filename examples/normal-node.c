8380e1155e7b10e6947787612ea5f1ed4f4aae79
bernardtran99 <bernardtran99@gmail.com>
2021-08-20 10:44:41 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -451,7 +451,7 @@ int main(int argc, char *argv[]) {
     
     ndn_lite_startup();
     //FACE NEEDS TO BE INITIATED WITH CORRECT PARAMETERS BEFORE SENDING OR RECEIVING ANCMT
-    //populate_fib();
+    populate_fib();
     ndn_name_from_string(&prefix_name, ancmt_string, strlen(ancmt_string));
     ndn_forwarder_register_name_prefix(&prefix_name, on_interest, NULL);
     //registers ancmt prefix with the forwarder so when ndn_forwarder_process is called, it will call the function on_interest
