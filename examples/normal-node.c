2fcb5d37a7803812b5fb28de51d49f452b19c225
bernardtran99 <bernardtran99@gmail.com>
2021-08-20 10:35:27 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -359,7 +359,7 @@ int on_interest(const uint8_t* interest, uint32_t interest_size, void* userdata)
             }
         }
     }
-    
+
     last_interest = timestamp;
     
     return NDN_FWD_STRATEGY_SUPPRESS;
@@ -451,7 +451,7 @@ int main(int argc, char *argv[]) {
     
     ndn_lite_startup();
     //FACE NEEDS TO BE INITIATED WITH CORRECT PARAMETERS BEFORE SENDING OR RECEIVING ANCMT
-    populate_fib();
+    //populate_fib();
     ndn_name_from_string(&prefix_name, ancmt_string, strlen(ancmt_string));
     ndn_forwarder_register_name_prefix(&prefix_name, on_interest, NULL);
     //registers ancmt prefix with the forwarder so when ndn_forwarder_process is called, it will call the function on_interest
