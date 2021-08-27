069ac26b4619fe307949282bb01cc6c428232e22
bernardtran99 <bernardtran99@gmail.com>
2021-08-27 10:27:24 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -307,9 +307,9 @@ int on_interest(const uint8_t* interest, uint32_t interest_size, void* userdata)
     char prefix_convert[10];
     int j = sprintf(prefix_convert, "%s", prefix);
     char *prefix_check = "ancmt";
-    printf("PREFIX: %s\n", prefix);
-    printf("PREFIX_CONVERT: %s\n", prefix_convert);
-    printf("PREFIX_CHECK: %s\n", prefix_check);
+    printf("PREFIX: `%s`\n", prefix);
+    printf("PREFIX_CONVERT: `%s`\n", prefix_convert);
+    printf("PREFIX_CHECK: `%s`\n", prefix_check);
     printf("PREFIX(int): %d\n", prefix);
     printf("PREFIX CONVERT(int): %d\n", prefix_convert);
     printf("PREFIX_CHECK(int): %d\n", prefix_check);
