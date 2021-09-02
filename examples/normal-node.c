5adce02b9dbecb388123abb842253d16b8070fa5
bernardtran99 <bernardtran99@gmail.com>
2021-09-02 19:33:07 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -457,17 +457,17 @@ void populate_incoming_fib() {
     // port2 = htons((uint16_t) ul_port);
     // face = ndn_udp_unicast_face_construct(INADDR_ANY, port1, server_ip, port2);
 
-    // sz_port1 = "5000";
-    // sz_addr = "rpi2-btran";
-    // sz_port2 = "3000";
-    // host_addr = gethostbyname(sz_addr);
-    // paddrs = (struct in_addr **)host_addr->h_addr_list;
-    // server_ip = paddrs[0]->s_addr;
-    // ul_port = strtoul(sz_port1, NULL, 10);
-    // port1 = htons((uint16_t) ul_port);
-    // ul_port = strtoul(sz_port2, NULL, 10);
-    // port2 = htons((uint16_t) ul_port);
-    // face = ndn_udp_unicast_face_construct(INADDR_ANY, port1, server_ip, port2);
+    sz_port1 = "5000";
+    sz_addr = "rpi2-btran";
+    sz_port2 = "3000";
+    host_addr = gethostbyname(sz_addr);
+    paddrs = (struct in_addr **)host_addr->h_addr_list;
+    server_ip = paddrs[0]->s_addr;
+    ul_port = strtoul(sz_port1, NULL, 10);
+    port1 = htons((uint16_t) ul_port);
+    ul_port = strtoul(sz_port2, NULL, 10);
+    port2 = htons((uint16_t) ul_port);
+    face = ndn_udp_unicast_face_construct(INADDR_ANY, port1, server_ip, port2);
 
     sz_port1 = "5000";
     sz_addr = "rpi3-btran";
