48348b63e6cd0ae63c72db33610d1ae8f0c8f697
bernardtran99 <bernardtran99@gmail.com>
2021-09-16 20:57:06 -0400

    Update debug-server.c

--- examples/debug-server.c
+++ examples/debug-server.c
@@ -53,6 +53,7 @@ void testTree() {
     struct Node *root;
     printf("here\n");
     root->data = "root";
+    printf("here\n");
     root->firstChild = addNode("192.168.1.1");
     root->firstChild->firstChild = addNode("192.168.1.2");
     root->firstChild->firstChild->nextSibling = addNode("192.168.1.3");
