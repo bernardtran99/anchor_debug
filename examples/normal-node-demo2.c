5ff695219a9449228360b430322a833800b567ca
bernardtran99 <bernardtran99@gmail.com>
2021-10-01 12:49:02 -0400

    Update normal-node-demo2.c

--- examples/normal-node-demo2.c
+++ examples/normal-node-demo2.c
@@ -346,7 +346,13 @@ int on_interest(const uint8_t* interest, uint32_t interest_size, void* userdata)
     // char prefix_convert[10];
     // int j = sprintf(prefix_convert, "%s", prefix);
     // char *prefix_check = "ancmt";
-    printf("PREFIX: %s\n", prefix);
+    printf("PREFIX: %s/", prefix);
+    prefix = &interest_pkt.name.components[1].value[0];
+    prefix = trimwhitespace(prefix);
+    printf("PREFIX: %s/", prefix);
+    prefix = &interest_pkt.name.components[2].value[0];
+    prefix = trimwhitespace(prefix);
+    printf("PREFIX: %s/", prefix);
     // printf("PREFIX_CONVERT: `%s`\n", prefix_convert);
     // printf("PREFIX_CHECK: `%s`\n", prefix_check);
     // printf("PREFIX(int): %d\n", prefix);
