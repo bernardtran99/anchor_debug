f47274a7fd038a9563ae86762628233d4788bb0c
bernardtran99 <bernardtran99@gmail.com>
2021-10-01 05:31:14 -0400

    Update change-link.py

--- change-link.py
+++ change-link.py
@@ -9,13 +9,9 @@ print(os.getcwd())
 with open(file, 'r+') as fd:
     contents = fd.readlines()
     print(contents[0])
-    print(enumerate(contents))
-#     if match_string in contents[-1]:  # Handle last line to prevent IndexError
-#         contents.append(insert_string)
-#     else:
-#         for index, line in enumerate(contents):
-#             if match_string in line and insert_string not in contents[index + 1]:
-#                 contents.insert(index + 1, insert_string)
-#                 break
-#     fd.seek(0)
-#     fd.writelines(contents)
+    for index, line in enumerate(contents):
+        if match_string in line and insert_string not in contents[index + 1]:
+            contents.insert(index + 1, insert_string)
+            break
+    fd.seek(0)
+    fd.writelines(contents)
