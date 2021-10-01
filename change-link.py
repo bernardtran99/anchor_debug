05ef72b319f8d6d37f06184fe4548d65e13253ab
bernardtran99 <bernardtran99@gmail.com>
2021-10-01 05:24:01 -0400

    Update change-link.py

--- change-link.py
+++ change-link.py
@@ -3,17 +3,16 @@ file = "~/Documents/anchor_debug/build/CMakeFiles/normal-node.dir/link.txt"
 match_string = ".o  "
 insert_string = "-pthread "
 print(os.getcwd())
-"""
-with open(file, 'r+') as fd:
-    contents = fd.readlines()
-    print(contents[0])
-    if match_string in contents[-1]:  # Handle last line to prevent IndexError
-        contents.append(insert_string)
-    else:
-        for index, line in enumerate(contents):
-            if match_string in line and insert_string not in contents[index + 1]:
-                contents.insert(index + 1, insert_string)
-                break
-    fd.seek(0)
-    fd.writelines(contents)
-"""
+
+# with open(file, 'r+') as fd:
+#     contents = fd.readlines()
+#     print(contents[0])
+#     if match_string in contents[-1]:  # Handle last line to prevent IndexError
+#         contents.append(insert_string)
+#     else:
+#         for index, line in enumerate(contents):
+#             if match_string in line and insert_string not in contents[index + 1]:
+#                 contents.insert(index + 1, insert_string)
+#                 break
+#     fd.seek(0)
+#     fd.writelines(contents)
