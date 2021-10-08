c227d4001ea5d2193a2871a511d7fab1440e3768
bernardtran99 <bernardtran99@gmail.com>
2021-10-08 08:22:59 -0400

    Update change-link-vm.py

--- change-link-vm.py
+++ change-link-vm.py
@@ -3,6 +3,7 @@ import os
 
 insert_string = " -pthread"
 # print(os.getcwd())
+#change pi to debug
 
 file = "/home/debug/Documents/anchor_debug/build/CMakeFiles/normal-node.dir/link.txt"
 with open(file, 'r+') as fd:
@@ -75,3 +76,63 @@ with open(file, 'r+') as fd:
     fd.seek(0)
     fd.writelines(string)
     print("Written to normal-node-demo5.dir")
+
+file = "/home/debug/Documents/anchor_debug/build/CMakeFiles/normal-node-demo6.dir/link.txt"
+with open(file, 'r+') as fd:
+    contents = fd.readlines()
+    print(contents)
+    string = contents[0].split()
+    string.insert(8, insert_string)
+    string = ' '.join(string)
+    print(string)
+    fd.seek(0)
+    fd.writelines(string)
+    print("Written to normal-node-demo6.dir")
+
+file = "/home/debug/Documents/anchor_debug/build/CMakeFiles/normal-node-demo7.dir/link.txt"
+with open(file, 'r+') as fd:
+    contents = fd.readlines()
+    print(contents)
+    string = contents[0].split()
+    string.insert(8, insert_string)
+    string = ' '.join(string)
+    print(string)
+    fd.seek(0)
+    fd.writelines(string)
+    print("Written to normal-node-demo7.dir")
+
+file = "/home/debug/Documents/anchor_debug/build/CMakeFiles/normal-node-demo8.dir/link.txt"
+with open(file, 'r+') as fd:
+    contents = fd.readlines()
+    print(contents)
+    string = contents[0].split()
+    string.insert(8, insert_string)
+    string = ' '.join(string)
+    print(string)
+    fd.seek(0)
+    fd.writelines(string)
+    print("Written to normal-node-demo8.dir")
+
+file = "/home/debug/Documents/anchor_debug/build/CMakeFiles/normal-node-demo9.dir/link.txt"
+with open(file, 'r+') as fd:
+    contents = fd.readlines()
+    print(contents)
+    string = contents[0].split()
+    string.insert(8, insert_string)
+    string = ' '.join(string)
+    print(string)
+    fd.seek(0)
+    fd.writelines(string)
+    print("Written to normal-node-demo9.dir")
+
+file = "/home/debug/Documents/anchor_debug/build/CMakeFiles/normal-node-demo10.dir/link.txt"
+with open(file, 'r+') as fd:
+    contents = fd.readlines()
+    print(contents)
+    string = contents[0].split()
+    string.insert(8, insert_string)
+    string = ' '.join(string)
+    print(string)
+    fd.seek(0)
+    fd.writelines(string)
+    print("Written to normal-node-demo10.dir")
