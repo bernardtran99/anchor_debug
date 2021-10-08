a5fd5d6150508ba48f951805fdf9fb8442903bdf
bernardtran99 <bernardtran99@gmail.com>
2021-10-08 12:21:31 -0400

    Update parse-debug.py

--- parse-debug.py
+++ parse-debug.py
@@ -2,6 +2,13 @@ import fileinput
 import os
 
 inputFile = "/home/debug/Documents/anchor_debug/build/debug-output.txt"
+outputFile = "/home/debug/Documents/anchor_debug/build/parsed-debug.txt"
+onInterest = ""
+nodeStart = ""
+isAnchor = ""
 with open(inputFile, 'r+') as fd:
-    contents = fd.readlines()
-    print(contents)
+    for line in fd:
+        print(line)
+        strings = line.strip()
+        print(strings)
+        
