dae2a003a4d9c32d70e73d5a28140eac3e7f38bb
bernardtran99 <bernardtran99@gmail.com>
2021-10-08 13:28:12 -0400

    Update parse-debug.py

--- parse-debug.py
+++ parse-debug.py
@@ -89,7 +89,7 @@ def node():
         nx.draw(G, pos, with_labels=True,node_size=node_sizes,edgecolors='black',node_color=node_colors,connectionstyle='arc3, rad = 0.1')        
         #display.clear_output(wait=True)
         #display.display(plt.gcf()) 
-        plt.imshow()
+        plt.show()
         time.sleep(1)
 
 node()
