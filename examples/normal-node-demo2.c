21174c3105bf99e58f42bd05c4f1c4c22a62545f
bernardtran99 <bernardtran99@gmail.com>
2021-10-08 11:08:04 -0400

    Update normal-node-demo2.c

--- examples/normal-node-demo2.c
+++ examples/normal-node-demo2.c
@@ -361,6 +361,7 @@ int on_interest(const uint8_t* interest, uint32_t interest_size, void* userdata)
     printf("%s\n", prefix);
     prefix = &interest_pkt.name.components[0].value[0];
     prefix = trimwhitespace(prefix);
+    printf("PREFIX[0]: %s\n", prefix);
 
     // for (int i = 0; i < interest_pkt.name.components_size; i++) {
     //     printf("/");
@@ -449,12 +450,12 @@ int on_interest(const uint8_t* interest, uint32_t interest_size, void* userdata)
 
     last_interest = current_time;
 
-    // prefix = &interest_pkt.name.components[2].value[0];
-    // prefix = trimwhitespace(prefix);
-    // char temp_message[80];
-    // strcat(temp_message, "On Interest: ");
-    // strcat(temp_message, prefix);
-    // send_debug_message(temp_message);
+    prefix = &interest_pkt.name.components[2].value[0];
+    prefix = trimwhitespace(prefix);
+    char temp_message[80];
+    strcat(temp_message, "On Interest: ");
+    strcat(temp_message, prefix);
+    send_debug_message(temp_message);
 
     printf("END OF ON_INTEREST\n");
     
