f7b79c544da6a2eb33888e23c2f1366031b6c0ef
bernardtran99 <bernardtran99@gmail.com>
2021-10-09 02:37:48 -0400

    Update parse-debug.py

--- parse-debug.py
+++ parse-debug.py
@@ -113,7 +113,7 @@ def generate_layer_2():
     plt.title("Layer 2 Tree")
     nx.draw(G, pos, with_labels=True,node_size=node_sizes,edgecolors='black',node_color=node_colors,connectionstyle='arc3, rad = 0.1')
     plt.show(block=False)
-    plt.pause(30)
+    plt.pause(1)
 
 while True:
     current_time += 0.003
