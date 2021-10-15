a9a7a4d9196163dc90be909002c92bedde038812
bernardtran99 <bernardtran99@gmail.com>
2021-10-15 00:43:59 -0400

    update

---
+++ examples/test2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <setjmp.h>
+#include "testfunc.h"
+
+int main() {
+    a();
+    return 0;
+}
---
+++ examples/testfunc.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <setjmp.h>
+
+void a();
