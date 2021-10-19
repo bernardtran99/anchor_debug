d8619185683593db0b2bcb748c4dd070ef3e5b50
bernardtran99 <bernardtran99@gmail.com>
2021-10-19 14:50:30 -0400

    Update debug-server.py

--- misc/debug-server.py
+++ misc/debug-server.py
@@ -16,6 +16,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             data = conn.recv(1024)
             print(data)
+            print(data.decode("utf-8"))
             if not data:
                 break
             conn.sendall(data)
