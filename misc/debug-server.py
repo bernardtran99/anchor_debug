763357988b9d2eed0340dd84f7c1f0fd223d9599
bernardtran99 <bernardtran99@gmail.com>
2021-10-19 14:49:35 -0400

    Update debug-server.py

--- misc/debug-server.py
+++ misc/debug-server.py
@@ -15,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
-            print(type(data))
+            print(data)
             if not data:
                 break
             conn.sendall(data)
