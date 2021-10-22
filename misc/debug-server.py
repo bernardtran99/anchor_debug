024353c36696269b32c35692ee519da7cb55772b
bernardtran99 <bernardtran99@gmail.com>
2021-10-21 22:50:10 -0400

    Update debug-server.py

--- misc/debug-server.py
+++ misc/debug-server.py
@@ -134,13 +134,10 @@ class EchoServerProtocol(asyncio.Protocol):
                 node9_list.append((prev_node9, node_num))
                 prev_node9 = node_num
 
-        edges = G.edges()
-        global colors
-        colors = [G[u][v]['color'] for u,v in edges]
+        colors = nx.get_edge_attributes(G,'color').values()
         plt.clf()
         plt.title(graph_title)
-        nx.draw(G, pos, with_labels=True,node_size=node_sizes,edgecolors='black',node_color=node_colors,connectionstyle='arc3, rad = 0.1')
-        nx.draw_networkx_edges(G, edge_color = colors)
+        nx.draw(G, pos, with_labels=True,node_size=node_sizes,edgecolors='black',edge_color = colors,node_color=node_colors,connectionstyle='arc3, rad = 0.1')
         plt.show(block=False)
         plt.pause(0.000001)
 
