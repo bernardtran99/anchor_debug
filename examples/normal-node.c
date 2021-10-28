5986bc53ceb16356305aa61ca7283f2051460a28
bernardtran99 <bernardtran99@gmail.com>
2021-10-28 19:39:23 -0400

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -1003,7 +1003,7 @@ int main(int argc, char *argv[]) {
     //FACE NEEDS TO BE INITIATED WITH CORRECT PARAMETERS BEFORE SENDING OR RECEIVING ANCMT
     //DEMO: CHANGE
     populate_incoming_fib();
-    callback_insert_data(on_data, fill_pit);
+    callback_insert(on_data, fill_pit);
     //registers ancmt prefix with the forwarder so when ndn_forwarder_process is called, it will call the function on_interest
     //populate_outgoing_fib();
 
