441d06f0f822514f7eeafef0c59c00c7714b6296
bernardtran99 <bernardtran99@gmail.com>
2021-11-12 08:44:21 -0500

    Update normal-node-demo2.c

--- examples/normal-node-demo2.c
+++ examples/normal-node-demo2.c
@@ -885,17 +885,18 @@ void fill_pit(const uint8_t* interest, uint32_t interest_size, ndn_face_intf_t *
     ndn_name_print(&interest_pkt.name);
 
     char *cmp_string = "";
-    cmp_string = get_prefix_component(interest_pkt.name, 0);
-    
+    cmp_string = get_prefix_component(interest_pkt.name, 0);    
 
     int third_slot;
     third_slot = atoi(get_prefix_component(interest_pkt.name, 2));
+
     printf("CMP STRING: _%s_\n",cmp_string);
 
     if(strcmp(cmp_string, "ancmt") == 0) {
         printf("1\n");
     }
     if(ancmt_num < max_interfaces) {
+        printf("CMP STRING: _%s_\n",cmp_string);
         printf("2\n");
     }
 
