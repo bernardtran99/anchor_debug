e094c37cf3e4cd788d5fed8e2c0addd7a8b716d3
bernardtran99 <bernardtran99@gmail.com>
2021-11-11 21:09:44 -0500

    update all

--- examples/normal-node-demo2.c
+++ examples/normal-node-demo2.c
@@ -494,7 +494,7 @@ void reply_ancmt() {
     printf("\nReply Ancmt...\n");
     char *reply[10];
     int counter = 0;
-
+    printf("here\n");
     for(int i = 0; i < node_anchor_pit.mem; i++) {
         char *check_ancmt = "";
         check_ancmt = get_prefix_component(node_anchor_pit.slots[i].name_struct, 0);
