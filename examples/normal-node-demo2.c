5d281d928a233378c8a629edbcd8774fbe16b085
bernardtran99 <bernardtran99@gmail.com>
2021-11-11 22:49:00 -0500

    Update normal-node-demo2.c

--- examples/normal-node-demo2.c
+++ examples/normal-node-demo2.c
@@ -509,10 +509,10 @@ void reply_ancmt() {
         if(strcmp(check_ancmt, "ancmt") == 0){
             printf("Ancmt found\n");
             reply[counter] = get_prefix_component(node_anchor_pit.slots[i].name_struct, 2);
-            printf("COUNTER: %d\n",counter)
+            printf("COUNTER: %d\n",counter);
             printf("REPLY COUNTER: %s\n", reply[counter]);
             counter++;
-            printf("COUNTER: %d\n",counter)
+            printf("COUNTER: %d\n",counter);
         }
     }
     //printf("2\n");
