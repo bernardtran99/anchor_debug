855828a3142bc1a112374096231b825f73084629
bernardtran99 <bernardtran99@gmail.com>
2021-11-17 16:31:08 -0500

    Update normal-node.c

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -688,7 +688,7 @@ ndn_udp_face_t *generate_udp_face(char* input_ip, char *port_1, char *port_2) {
         port2 = htons((uint16_t) ul_port);
         face = ndn_udp_unicast_face_construct(INADDR_ANY, port1, server_ip, port2);
         printf("Added face to table\n");
-        udp_table.faces[0] = face;
+        *udp_table.faces[0] = face;
     }
 
     return face;
