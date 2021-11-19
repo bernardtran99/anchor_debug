c8e4fa355cbc77b83d79fe193857b38d878be937
bernardtran99 <bernardtran99@gmail.com>
2021-11-19 13:07:29 -0500

    Revert "Update ndn-constants.h"
    This reverts commit b64b52f0475a85e696c08591ade88041d62a89ef.

--- ndn-lite/ndn-constants.h
+++ ndn-lite/ndn-constants.h
@@ -46,7 +46,7 @@ typedef uint16_t ndn_table_id_t;
 #define NDN_PIT_MAX_SIZE 32
 #define NDN_CS_MAX_SIZE 10
 //btran changed NDN_FACE_TABLE_MAX_SIZE to 30 from 10
-#define NDN_FACE_TABLE_MAX_SIZE 10
+#define NDN_FACE_TABLE_MAX_SIZE 30
 #define NDN_FACE_DEFAULT_COST 1
 #define NDN_AES_BLOCK_SIZE 16
 #define NDN_MAX_FACE_PER_PIT_ENTRY 3
