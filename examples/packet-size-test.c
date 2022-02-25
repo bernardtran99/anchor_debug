df84092ee703877d6686878a5d2abc3b0f0f197d
bernardtran99 <bernardtran99@gmail.com>
2022-02-25 17:21:56 -0500

    Update packet-size-test.c

--- examples/packet-size-test.c
+++ examples/packet-size-test.c
@@ -40,7 +40,7 @@ int main() {
     printf("Char: %lu\n", sizeof(char));
     printf("Int: %lu\n", sizeof(int));
     printf("Float: %lu\n", sizeof(float));
-    printf("Double: %lu", sizeof(double));
+    printf("Double: %lu\n", sizeof(double));
     printf("Interest Size: %lu\n", sizeof(interest_packet));
     printf("Data Size: %lu\n", sizeof(data_packet));
 
