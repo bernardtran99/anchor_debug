5d22082f51c8ffd02772a29ec5669926a8e0fb85
bernardtran99 <bernardtran99@gmail.com>
2022-03-23 16:08:37 -0400

    update all

--- examples/normal-node-demo3.c
+++ examples/normal-node-demo3.c
@@ -1509,7 +1509,6 @@ void *command_process(void *var) {
                 if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
                 {
                     printf("\n Socket creation error \n");
-                    return -1;
                 }
 
                 // int flags = 1;
@@ -1524,13 +1523,11 @@ void *command_process(void *var) {
                 if(inet_pton(AF_INET, DEBUG, &serv_addr.sin_addr)<=0) 
                 {
                     printf("\nInvalid address/ Address not supported \n");
-                    return -1;
                 }
             
                 if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
                 {
                     printf("\nConnection Failed \n");
-                    return -1;
                 }
 
                 debug_connected = true;
