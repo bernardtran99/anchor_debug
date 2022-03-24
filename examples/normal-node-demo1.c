e7b8f9ff7c95158e05ec6ff775c1d0e90eb4ab15
bernardtran99 <bernardtran99@gmail.com>
2022-03-23 21:37:35 -0400

    update all

--- examples/normal-node-demo1.c
+++ examples/normal-node-demo1.c
@@ -1221,7 +1221,19 @@ void on_data(const uint8_t* rawdata, uint32_t data_size, void* userdata) {
     prefix = get_string_prefix(data.name);
     printf("%s\n", prefix); 
     printf("DATA CONTENT: [%s]\n", data.content_value);
-    printf("Content Size Field: %d", data.content_size);
+    printf("Content Size Field: %d\n", data.content_size);
+    for(int i = 0; i < data.content_size; i++) {
+        printf("%c", data.content_value[i]);
+    }
+    printf("\n");
+    for(int i = 0; i < data.content_size; i++) {
+        printf("%s", data.content_value[i]);
+    }
+    printf("\n");
+    for(int i = 0; i < data.content_size; i++) {
+        printf("%d", data.content_value[i]);
+    }
+    printf("\n");
     printf("Packet Size: %d\n", data_size);
 
     char temp_message[80] = "";
