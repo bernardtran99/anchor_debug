07141c5aa9f1ca6c003a85e42ab4fc1f9d91f69b
bernardtran99 <bernardtran99@gmail.com>
2022-03-25 21:18:44 -0400

    update all

--- examples/normal-node.c
+++ examples/normal-node.c
@@ -245,7 +245,7 @@ void add_neighbor(int neighbor_num) {
     for(size_t i = 0; i < nl_size; i++) {
         if(neighbor_list[i] == 0) {
             neighbor_list[i] = neighbor_num;
-            break;
+            return;
         }
     }
 }
