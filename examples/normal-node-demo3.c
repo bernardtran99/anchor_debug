3328fecb6be7ab7f9e84568c9fc0a4692d7f09cc
bernardtran99 <bernardtran99@gmail.com>
2022-04-01 13:37:13 -0400

    update all in tri position

--- examples/normal-node-demo3.c
+++ examples/normal-node-demo3.c
@@ -1684,10 +1684,10 @@ int main(int argc, char *argv[]) {
 
     //This is for adding 2 way neighbors in network
     //DEMO: CHANGE
-    node_num = 1;
-    add_neighbor(5);
-    add_neighbor(7);
-    add_neighbor(10);
+    node_num = 3;
+    add_neighbor(1);
+    add_neighbor(2);
+    add_neighbor(4);
 
     last_interest = ndn_time_now_ms();
     
--- examples/normal-node-demo4.c
+++ examples/normal-node-demo4.c
@@ -1684,10 +1684,8 @@ int main(int argc, char *argv[]) {
 
     //This is for adding 2 way neighbors in network
     //DEMO: CHANGE
-    node_num = 1;
-    add_neighbor(5);
-    add_neighbor(7);
-    add_neighbor(10);
+    node_num = 4;
+    add_neighbor(3);
 
     last_interest = ndn_time_now_ms();
     
