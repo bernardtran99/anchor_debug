41db3fc74e604b8f6930a620a99780536e96e789
bernardtran99 <bernardtran99@gmail.com>
2022-04-22 14:04:33 -0400

    Update example-graph.py

--- misc/example-graph.py
+++ misc/example-graph.py
@@ -92,6 +92,7 @@ node_colors = ['green']*10
 
 G.add_edges_from([(4, 5), (4, 7), (4, 6), (5, 7), (5, 8), (6, 7), (6, 9)])
 plt.clf()
+plt.figure(1)
 plt.title("POS TEST")
 nx.draw(G, pos, with_labels=True, node_size=node_sizes, edgecolors='black', node_color=node_colors, connectionstyle='arc3, rad = 0.1')
 plt.show(block=False)
