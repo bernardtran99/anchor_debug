9b40ab7f3f992a596fff264c0a3cd4392dbc377c
bernardtran99 <bernardtran99@gmail.com>
2022-04-22 13:42:17 -0400

    Update example-graph.py

--- misc/example-graph.py
+++ misc/example-graph.py
@@ -85,6 +85,6 @@ node_colors = ['green'] * 10
 
 plt.clf()
 plt.title("POS TEST")
-nx.draw(G, pos, with_labels=True,node_size=node_sizes,edgecolors='black', edge_color = "black", node_color=node_colors,connectionstyle='arc3, rad = 0.1')
+nx.draw(G, pos, with_labels=True, node_size=node_sizes, edgecolors='black', node_color=node_colors, connectionstyle='arc3, rad = 0.1')
 plt.show(block=False)
 plt.pause(0.000001)
