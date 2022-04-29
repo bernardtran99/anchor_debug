8192fef983c717743e1f091e740d251a9925bd15
bernardtran99 <bernardtran99@gmail.com>
2022-04-29 12:26:05 -0400

    Update ndn-constants.h

--- ndn-lite/ndn-constants.h
+++ ndn-lite/ndn-constants.h
@@ -41,9 +41,9 @@
 typedef uint16_t ndn_table_id_t;
 
 #define NDN_INVALID_ID 0xFFFF
-#define NDN_NAMETREE_MAX_SIZE 64
+#define NDN_NAMETREE_MAX_SIZE 256
 #define NDN_FIB_MAX_SIZE 20
-#define NDN_PIT_MAX_SIZE 32
+#define NDN_PIT_MAX_SIZE 256
 #define NDN_CS_MAX_SIZE 10
 //number of faces allowed in face table
 #define NDN_FACE_TABLE_MAX_SIZE 1024
