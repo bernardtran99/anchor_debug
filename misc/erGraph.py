5abadac515c178c8fd1d1dd43ef5234dc3f90509
bernardtran99 <bernardtran99@gmail.com>
2022-05-06 17:48:51 -0400

    Update erGraph.py

--- misc/erGraph.py
+++ misc/erGraph.py
@@ -78,7 +78,7 @@ for i in range(0,14):
     for j in range(0,14):
         if Graph_Matrix[i][j] == 1:
             print("%d " % j)
-            G.add_edge(i, j) 
+            G.add_edges_from([(i,j)]) 
 
 plt.clf()
 plt.title("POS TEST")
